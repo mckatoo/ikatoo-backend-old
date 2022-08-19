f6969946582f0965a73e4f066bd299ec3704b4f4
Milton Carlos Katoo <mckatoo@gmail.com>
2022-08-19 19:16:29 -0300

    added crud methods in the interface

--- src/domain/menu/menu.repository.ts
+++ src/domain/menu/menu.repository.ts
@@ -1,7 +1,10 @@
-import { Menu, MenuItem } from "./menu.entity";
+import { Menu, MenuItem, MenuProps } from "./menu.entity";
 
 export interface MenuRepositoryInterface {
   create(menu: Menu): Promise<void>;
-  insertItem(item: MenuItem, menuId: string): Promise<void>;
-  getAll(): Promise<Menu[]>;
+  rename(new_name: string, menu_id: string): Promise<void>;
+  insertItem(item: MenuItem, menu_id: string): Promise<void>;
+  removeItem(item_id: string): Promise<void>;
+  updateItem(item_id: string, item: MenuItem): Promise<void>
+  get(userId: string, name: string): Promise<MenuProps>;
 }

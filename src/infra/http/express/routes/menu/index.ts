64e8dfcbea9bc137295ca98d8e67fd7bb9dfe39d
Milton Carlos Katoo <mckatoo@gmail.com>
2022-08-19 21:04:29 -0300

    create menu route with usecase implementation

--- src/infra/http/express/routes/menu/index.ts
+++ src/infra/http/express/routes/menu/index.ts
@@ -1,10 +1,11 @@
+import { CreateMenuUseCase } from "@application/menu/create/create-menu.use-case";
 import { MenuRepository } from "@infra/db/menu";
 import { Router, Request, Response } from "express"
 import { mockedPublicMenu } from "./mock";
 
 const menuRoute = Router();
 
-const menuPageRepository = new MenuRepository();
+const menuRepository = new MenuRepository();
 
 menuRoute.get("/menu/:title", (req: Request, res: Response) => {
   const { title } = req.params
@@ -12,10 +13,12 @@ menuRoute.get("/menu/:title", (req: Request, res: Response) => {
   res.status(200).json(mockedPublicMenu)
 })
 
-menuRoute.post("/menu", (req: Request, res: Response) => {
+menuRoute.post("/menu", async (req: Request, res: Response) => {
   const { menu } = req.body
+  const createUseCase = new CreateMenuUseCase(menuRepository);
+  const output = await createUseCase.execute(menu);
 
-  res.status(201).json(mockedPublicMenu)
+  res.status(201).json(output)
 })
 
 export default menuRoute

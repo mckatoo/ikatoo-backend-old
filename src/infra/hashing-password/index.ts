1147218e6103021727f1b759b28a7932440f56b7
Milton Carlos Katoo <mckatoo@gmail.com>
2022-08-25 13:25:38 -0300

    changed export way to named export

--- src/infra/hashing-password/index.ts
+++ src/infra/hashing-password/index.ts
@@ -1,11 +1,13 @@
 import * as bcrypt from "bcrypt";
 
-export const hashPassword = async (saltRounds: number, password: string) => {
+const hashPassword = async (saltRounds: number, password: string) => {
   const salt = await bcrypt.genSalt(saltRounds);
   const hashedPassword = await bcrypt.hash(password, salt);
 
   return `${hashedPassword}`;
 };
 
-export const comparePassword = async (password: string, hash: string) =>
+const comparePassword = async (password: string, hash: string) =>
   await bcrypt.compare(password, hash);
+
+export { hashPassword, comparePassword };

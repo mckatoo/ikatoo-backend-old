2b4c687c2d0393269205ae4de4db22570c8d6a31
Milton Carlos Katoo <mckatoo@gmail.com>
2022-08-25 18:14:43 -0300

    create menu with id

--- src/application/menu/create/create-menu.use-case.test.ts
+++ src/application/menu/create/create-menu.use-case.test.ts
@@ -1,6 +1,5 @@
-import { Menu, MenuProps } from "@domain/menu/menu.entity";
 import { MenuRepository } from "@infra/db/menu";
-import { randomUUID } from "crypto";
+
 import { CreateMenuUseCase } from "./create-menu.use-case";
 
 describe("Create Menu use-case Test", () => {
@@ -8,37 +7,34 @@ describe("Create Menu use-case Test", () => {
   const createUseCase = new CreateMenuUseCase(repository);
 
   it("should create a new menu", async () => {
-    const menu = new Menu({
+    expect(await repository.count()).toBe(0);
+
+    const output = await createUseCase.execute({
       name: "public",
       items: [
         { label: "first item", to: "first/url" },
         { label: "second item", to: "second/url" },
       ],
-      user_id: randomUUID(),
+      user_id: "other_user_id",
     });
-
-    expect(await repository.count()).toBe(0);
-
-    const output = await createUseCase.execute(menu);
-    const expectedMenu = await repository.get(menu.user_id, "public");
+    const expectedMenu = await repository.get("other_user_id", "public");
     expect(output).toStrictEqual(expectedMenu);
     expect(await repository.count()).toBe(1);
   });
 
   it("should create a new menu with id", async () => {
-    const menu = new Menu({
+    expect(await repository.count()).toBe(1);
+
+    const output = await createUseCase.execute({
+      id: "teste_id",
       name: "public",
       items: [
         { label: "first item", to: "first/url" },
         { label: "second item", to: "second/url" },
       ],
-      user_id: randomUUID(),
+      user_id: "userId-test",
     });
-
-    expect(await repository.count()).toBe(1);
-
-    const output = await createUseCase.execute(menu);
-    const expectedMenu = await repository.get(menu.user_id, "public");
+    const expectedMenu = await repository.get("userId-test", "public");
 
     expect(output).toHaveProperty("id", "teste_id");
     expect(output).toStrictEqual(expectedMenu);
--- src/application/menu/create/create-menu.use-case.ts
+++ src/application/menu/create/create-menu.use-case.ts
@@ -1,24 +1,20 @@
-import { Menu, MenuItem } from "@domain/menu/menu.entity";
+import { Menu, MenuItem, MenuProps } from "@domain/menu/menu.entity";
 import { MenuRepositoryInterface } from "@domain/menu/menu.repository";
 
-type CreateMenuInput = {
-  name: string
-  items: MenuItem[]
-  user_id: string
-};
+type CreateMenuInput = MenuProps & { id?: string };
 
 type CreateMenuOutput = {
-  id: string
-  name: string
-  items: MenuItem[]
-  user_id: string
+  id: string;
+  name: string;
+  items: MenuItem[];
+  user_id: string;
 };
 
 export class CreateMenuUseCase {
   constructor(private menuRepository: MenuRepositoryInterface) {}
 
   async execute(input: CreateMenuInput): Promise<CreateMenuOutput> {
-    const menu = Menu.create(input);
+    const menu = Menu.create({ ...input }, input.id);
     await this.menuRepository.create(menu);
 
     return menu.toJson();

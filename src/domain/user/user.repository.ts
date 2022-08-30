660bd73c5c26d32c30e3f60ba1485cec3a344206
Milton Carlos Katoo <mckatoo@gmail.com>
2022-08-30 13:06:42 -0300

    implemented user sqlite repository

--- src/domain/user/user.repository.ts
+++ src/domain/user/user.repository.ts
@@ -1,10 +1,13 @@
 import { UserProps } from "./user.entity";
 
-export type UserWithId = UserProps & { id: string };
+export type UserWithId = UserProps & { id?: string };
 
 export interface UserRepositoryInterface {
-  create(user: UserProps): Promise<void>;
+  create(user: UserWithId): Promise<void>;
   getByUsername(username: string): Promise<UserWithId>;
   getByEmail(email: string): Promise<UserWithId>;
   searchByName(partialName: string): Promise<UserWithId[]>;
+  getAll(): Promise<UserWithId[]>;
+  update(user: UserProps, id: string): Promise<void>;
+  remove(id: string): Promise<void>;
 }
--- src/infra/db/user/sqlite/user-sqlite.repository.ts
+++ src/infra/db/user/sqlite/user-sqlite.repository.ts
@@ -4,39 +4,110 @@ import {
   UserWithId,
 } from "@domain/user/user.repository";
 import { randomUUID } from "crypto";
-import database, { SqliteConnection } from "./database";
 
-const db = database();
+import database from "./database";
 
 export class UserSqliteRepository implements UserRepositoryInterface {
-  async create(user: UserProps): Promise<void> {
-    (await db).exec(`create table users (
-      id text, 
-      name text, 
-      username text, 
-      password text, 
-      email text
+  async create(user: UserWithId): Promise<void> {
+    const db = await database();
+
+    await db.exec(`create table if not exists users (
+      id text NOT NULL UNIQUE PRIMARY KEY, 
+      name text NOT NULL UNIQUE, 
+      username text NOT NULL UNIQUE, 
+      password text NOT NULL, 
+      email text NOT NULL UNIQUE
       )`);
 
-    (await db).exec(`insert into users values(?,?,?,?,?)`, [
-      randomUUID(),
+    await db.run(
+      `insert into users values(?,?,?,?,?)`,
+      user.id || randomUUID(),
+      user.name,
+      user.username,
+      user.password,
+      user.email
+    );
+
+    db.close();
+  }
+
+  async getByUsername(username: string): Promise<UserWithId> {
+    const db = await database();
+    const user = await db.get<UserWithId>(
+      `select * from users where username = $username`,
+      { $username: username }
+    );
+    db.close();
+
+    if (!user) throw new Error("User not found");
+
+    return user;
+  }
+
+  async getByEmail(email: string): Promise<UserWithId> {
+    const db = await database();
+    const user = await db.get<UserWithId>(
+      "select * from users where email = $email",
+      {
+        $email: email,
+      }
+    );
+    db.close();
+
+    if (!user) throw new Error("User not found");
+
+    return user;
+  }
+
+  async searchByName(partialName: string): Promise<UserWithId[]> {
+    const db = await database();
+    const users = await db.all<UserWithId[]>(
+      `select * from users where name like '%${partialName}%'`
+    );
+    db.close();
+
+    if (!users) throw new Error("User not found");
+
+    return users;
+  }
+
+  async getAll(): Promise<UserWithId[]> {
+    const db = await database();
+    const users = await db.all<UserWithId[]>("select * from users");
+    db.close();
+
+    if (!users) throw new Error("User not found");
+
+    return users;
+  }
+
+  async update(user: UserProps, id: string): Promise<void> {
+    const db = await database();
+
+    await db.run(
+      `update users set 
+      id = ?,
+      name = ?,
+      username = ?,
+      password = ?,
+      email = ?
+      where id = ?`,
+      id,
       user.name,
       user.username,
       user.password,
       user.email,
-    ]);
-    (await db).close();
+      id
+    );
+
+    db.close();
   }
 
-  async getByUsername(username: string): Promise<UserWithId> {
-    throw new Error("Method not implemented.");
-  }
+  async remove(id: string): Promise<void> {
+    const db = await database();
 
-  getByEmail(email: string): Promise<UserWithId> {
-    throw new Error("Method not implemented.");
-  }
+    await db.run(`delete from users where id=?`, id);
 
-  searchByName(partialName: string): Promise<UserWithId[]> {
-    throw new Error("Method not implemented.");
+    db.close();
   }
 }

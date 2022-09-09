9196acf19b6a81da4a5a873c7172a501b7fa77a4
Milton Carlos Katoo <mckatoo@gmail.com>
2022-09-09 16:15:53 -0300

    env with error when key is undefined

--- src/infra/env.ts
+++ src/infra/env.ts
@@ -5,5 +5,6 @@ dotenv.config({
 })
 
 export const env = (key: string) => {
+  if (process.env[key] === undefined) console.error(`*** ${key} IS UNDEFINED. ***`)
   return `${process.env[key] ?? ''}`
 }

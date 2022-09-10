f5f3f0435af964f87837c5ce4201e1ea95b717f0
Milton Carlos Katoo <mckatoo@gmail.com>
2022-09-10 18:08:59 -0300

    🥅 (refactor): catch conflictor error

--- src/application/user/create/create-user.use-case.ts
+++ src/application/user/create/create-user.use-case.ts
@@ -1,3 +1,4 @@
+import { ConflictError } from '@application/helpers/api-erros'
 import { User, UserProps } from '@domain/user/user.entity'
 import { UserRepositoryInterface } from '@domain/user/user.repository'
 import { hashPassword } from '@infra/hashing-password'
@@ -7,14 +8,16 @@ type CreateUserInput = UserProps & { id?: string }
 type CreateUserOutput = Omit<UserProps, 'password'> & { id: string }
 
 export class CreateUserUseCase {
-  constructor (private readonly menuRepository: UserRepositoryInterface) {}
+  constructor (private readonly repository: UserRepositoryInterface) {}
 
   async execute (input: CreateUserInput): Promise<CreateUserOutput> {
+    const userExists = await this.repository.getByEmail(input.email)
+    if (userExists != null) throw new ConflictError('User already exists')
     const user = User.create(
       { ...input, password: await hashPassword(10, input.password) },
       input.id
     )
-    await this.menuRepository.create(user)
+    await this.repository.create(user)
 
     return {
       id: user.id,

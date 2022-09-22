40e2254b8577ae9b443a1f0c0779877ff459513e
Milton Carlos Katoo <mckatoo@gmail.com>
2022-09-22 19:31:09 -0300

    ✅ (test): add tests for to jsonwebtoken

---
+++ src/infra/jwt/jsonwebtoken/jsonwebtoken.test.ts
@@ -0,0 +1,47 @@
+import { TokenExpiredError } from 'jsonwebtoken'
+import { generateString } from '../../generate'
+import isValid from './isValid'
+import sign from './sign'
+
+describe('JsonWebToken Test', () => {
+  it('should create a token', async () => {
+    const token = sign({
+      userId: generateString(),
+      expiresIn: '60s'
+    })
+
+    expect(token.split('.')).toHaveLength(3)
+  })
+
+  it('should check if is valid token', async () => {
+    const token = sign({
+      userId: generateString(),
+      expiresIn: '60s'
+    })
+    expect(isValid(token)).toBe(true)
+  })
+
+  it('should invalidate token after corruption', async () => {
+    const token = sign({
+      userId: generateString(),
+      expiresIn: '60s'
+    })
+    expect(() => isValid(token.replace(token[3], ')'))).toThrowError('invalid token')
+  })
+
+  it('should invalidate token after expiration', async () => {
+    const token = sign({
+      userId: generateString(),
+      expiresIn: '60s'
+    })
+
+    expect(isValid(token)).toBe(true)
+
+    jest.useFakeTimers()
+    jest.spyOn(global, 'setTimeout')
+    setTimeout(async () => {
+      expect(() => isValid(token)).toThrowError(TokenExpiredError)
+    }, 60000)
+    jest.runAllTimers()
+  })
+})

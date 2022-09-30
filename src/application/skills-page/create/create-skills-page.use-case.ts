ee99b48dc7fee0ee229711998d6ea25a658b7124
Milton Carlos Katoo <mckatoo@gmail.com>
2022-09-30 11:47:30 -0300

    ✨ (feature): create skills page use-case

---
+++ src/application/skills-page/create/create-skills-page.use-case.ts
@@ -0,0 +1,26 @@
+import { ConflictError } from '@application/helpers/api-erros'
+import { SkillsPage, SkillsPageProps } from '@domain/skills-page/skills-page.entity'
+import { SkillsPageRepositoryInterface, SkillsPageWithId } from '@domain/skills-page/skills-page.repository'
+
+type CreateSkillsPageInput = SkillsPageProps & { id?: string }
+
+export class CreateSkillsPageUseCase {
+  constructor (private readonly repository: SkillsPageRepositoryInterface) {}
+
+  async execute (input: CreateSkillsPageInput): Promise<Required<SkillsPageWithId>> {
+    try {
+      const skillsPage = SkillsPage.create(input, input.id)
+      await this.repository.create(skillsPage)
+
+      return skillsPage.toJson()
+    } catch (error) {
+      if (
+        error instanceof Error &&
+        error.message === 'SQLITE_CONSTRAINT: UNIQUE constraint failed: skillsPages.user_id'
+      ) {
+        throw new ConflictError('This skills page already exists for this user')
+      }
+      throw error
+    }
+  }
+}

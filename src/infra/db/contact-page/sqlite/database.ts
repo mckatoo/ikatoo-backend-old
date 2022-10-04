67db11e6af7b679658f73c49cc4bae12bb2ccff6
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-04 20:10:06 -0300

    ✨ (feature): create database structure of the contact-page

---
+++ src/infra/db/contact-page/sqlite/database.ts
@@ -0,0 +1,18 @@
+import { initDb } from '@infra/db/sqlite'
+import { Database as DatabaseSqlite } from 'sqlite'
+import { Database, Statement } from 'sqlite3'
+
+export type SqliteConnection = DatabaseSqlite<Database, Statement>
+
+export default async () => {
+  const db = await initDb()
+
+  await db.exec(`create table if not exists contactsPages (
+    id text NOT NULL UNIQUE PRIMARY KEY, 
+    title text NOT NULL, 
+    description text NOT NULL, 
+    user_id text NOT NULL UNIQUE 
+    )`)
+
+  return db
+}

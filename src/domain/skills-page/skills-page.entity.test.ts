a8c9fe85645bf678c16dcd06808bccda8995ac47
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-04 10:09:26 -0300

    required props and id

--- src/domain/skills-page/skills-page.entity.test.ts
+++ src/domain/skills-page/skills-page.entity.test.ts
@@ -2,12 +2,6 @@ import { generateString } from '@infra/generate'
 import { SkillsPage, SkillsPageProps } from './skills-page.entity'
 
 describe('Test SkillsPage Test', () => {
-  it('should return error on instance without props', () => {
-    const skillsPageError = () => SkillsPage.create()
-
-    expect(skillsPageError).toThrow('Props required on contructor')
-  })
-
   it('constructor without id', () => {
     const skillsPageData: SkillsPageProps = {
       title: generateString(),
@@ -25,41 +19,13 @@ describe('Test SkillsPage Test', () => {
 
   it('should constructor with id', () => {
     const skillsPageData: SkillsPageProps = {
-      title: generateString(),
-      description: generateString(),
-      user_id: generateString()
-    }
-    const skillsPage = SkillsPage.create(skillsPageData, 'test_id')
-
-    expect(skillsPage.id).toBe('test_id')
-    expect(skillsPage.toJson()).toEqual({
-      id: skillsPage.id,
-      ...skillsPageData
-    })
-  })
-
-  it('should update title method', () => {
-    const skillsPageData: SkillsPageProps = {
+      id: generateString(),
       title: generateString(),
       description: generateString(),
       user_id: generateString()
     }
     const skillsPage = SkillsPage.create(skillsPageData)
-    skillsPage.updateTitle('new title')
 
-    expect(skillsPage.title).toBe('new title')
-  })
-
-  it('should update description method', () => {
-    const skillsPageData: SkillsPageProps = {
-      title: generateString(),
-      description: generateString(),
-      user_id: generateString()
-    }
-    const skillsPage = SkillsPage.create(skillsPageData)
-    const newDescription = generateString()
-    skillsPage.updateDescription(newDescription)
-
-    expect(skillsPage.description).toBe(newDescription)
+    expect(skillsPage.toJson()).toEqual(skillsPageData)
   })
 })

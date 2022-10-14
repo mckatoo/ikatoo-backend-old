87a164dad4ddbe13318e1dd0cc78bc95dc52835e
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-14 20:56:01 -0300

    ✨ (feature): github route
    get access token

--- src/infra/http/express/routes/auth/auth.test.ts
+++ src/infra/http/express/routes/auth/auth.test.ts
@@ -4,16 +4,22 @@ import request from 'supertest'
 import { CreateUserUseCase } from '@application/user/create/create-user.use-case'
 import { UserRepository } from '@infra/db/user'
 import { generateString } from '@infra/generate'
+import auth from '@infra/github/auth'
 import { Request, Response } from 'express'
 import { decodeToken } from './decodeToken'
 import { expressVerifyToken } from './verifyToken'
+
+jest.mock('axios')
+jest.mock('@infra/github/auth')
 
 describe('Express - Auth', () => {
   const repository = new UserRepository()
   const createUseCase = new CreateUserUseCase(repository)
 
-  app.get('/test', expressVerifyToken, (req: Request, res: Response) => {
-    res.status(200).send()
+  beforeAll(() => {
+    app.get('/test', expressVerifyToken, (req: Request, res: Response) => {
+      res.status(200).send()
+    })
   })
 
   it('should authenticate a valid username', async () => {
@@ -25,10 +31,12 @@ describe('Express - Auth', () => {
       password: 'teste12345',
       domain: `${generateString()}.com.br`
     })
-    const response = await request(app).post('/auth').send({
-      username: user?.username,
-      password: 'teste12345'
-    })
+    const response = await request(app)
+      .post('/auth')
+      .send({
+        username: user?.username,
+        password: 'teste12345'
+      })
 
     expect(response.status).toBe(200)
     expect(response.body).toHaveProperty('accessToken')
@@ -36,10 +44,12 @@ describe('Express - Auth', () => {
   })
 
   it('should not authenticate a invalid user', async () => {
-    const response = await request(app).post('/auth').send({
-      username: 'invalid-username',
-      password: 'invalid-pass'
-    })
+    const response = await request(app)
+      .post('/auth')
+      .send({
+        username: 'invalid-username',
+        password: 'invalid-pass'
+      })
 
     expect(response.status).toBe(401)
     expect(response.body.message).toBe('Credentials invalid.')
@@ -54,10 +64,12 @@ describe('Express - Auth', () => {
       password: 'teste12345',
       domain: `${generateString()}.com.br`
     })
-    const response = await request(app).post('/auth').send({
-      username: user?.username,
-      password: 'teste12345'
-    })
+    const response = await request(app)
+      .post('/auth')
+      .send({
+        username: user?.username,
+        password: 'teste12345'
+      })
 
     const accessToken: string = response.body.accessToken
     const decodedAccessToken = decodeToken(accessToken)
@@ -78,18 +90,20 @@ describe('Express - Auth', () => {
       password: 'teste12345',
       domain: `${generateString()}.com.br`
     })
-    const response = await request(app).post('/auth').send({
-      username: user?.username,
-      password: 'teste12345'
-    })
+    const response = await request(app)
+      .post('/auth')
+      .send({
+        username: user?.username,
+        password: 'teste12345'
+      })
 
     const accessToken: string = response.body.accessToken
     const decodedAccessToken = decodeToken(accessToken)
 
     expect(decodedAccessToken.userId).toBe(user?.id)
-    expect(
-      decodedAccessToken.expiresIn - decodedAccessToken.generatedAt
-    ).toBe(60 * 60)
+    expect(decodedAccessToken.expiresIn - decodedAccessToken.generatedAt).toBe(
+      60 * 60
+    )
   })
 
   it('should get refreshToken with expiration time in 2 days', async () => {
@@ -101,17 +115,19 @@ describe('Express - Auth', () => {
       password: 'teste12345',
       domain: `${generateString()}.com.br`
     })
-    const response = await request(app).post('/auth').send({
-      username: user?.username,
-      password: 'teste12345'
-    })
+    const response = await request(app)
+      .post('/auth')
+      .send({
+        username: user?.username,
+        password: 'teste12345'
+      })
 
     const refreshToken = response.body.refreshToken
     const decodedRefreshToken = decodeToken(refreshToken)
 
     expect(
       decodedRefreshToken.expiresIn - decodedRefreshToken.generatedAt
-    ).toBe(((60 * 60) * 24) * 2)
+    ).toBe(60 * 60 * 24 * 2)
   })
 
   it('should get new accessTokens and refreshToken using the refreshToken', async () => {
@@ -123,12 +139,15 @@ describe('Express - Auth', () => {
       password: 'teste12345',
       domain: `${generateString()}.com.br`
     })
-    const response = await request(app).post('/auth').send({
-      username: user?.username,
-      password: 'teste12345'
-    })
+    const response = await request(app)
+      .post('/auth')
+      .send({
+        username: user?.username,
+        password: 'teste12345'
+      })
 
-    const responseRefreshToken = await request(app).post('/refresh-token')
+    const responseRefreshToken = await request(app)
+      .post('/auth/refresh-token')
       .auth(response.body.refreshToken, { type: 'bearer' })
       .send()
     const { accessToken, refreshToken } = responseRefreshToken.body
@@ -149,17 +168,21 @@ describe('Express - Auth', () => {
       password: 'teste12345',
       domain: `${generateString()}.com.br`
     })
-    const responseAuth = await request(app).post('/auth').send({
-      username: user?.username,
-      password: 'teste12345'
-    })
+    const responseAuth = await request(app)
+      .post('/auth')
+      .send({
+        username: user?.username,
+        password: 'teste12345'
+      })
 
-    const responseRefreshToken = await request(app).post('/refresh-token')
+    const responseRefreshToken = await request(app)
+      .post('/auth/refresh-token')
       .auth(responseAuth.body.refreshToken, { type: 'bearer' })
       .send()
     const { accessToken } = responseRefreshToken.body
 
-    const response = await request(app).get('/test')
+    const response = await request(app)
+      .get('/test')
       .auth(accessToken, { type: 'bearer' })
       .send()
 
@@ -176,12 +199,15 @@ describe('Express - Auth', () => {
       domain: `${generateString()}.com.br`
     })
 
-    const { body } = await request(app).post('/auth').send({
-      username: user?.username,
-      password: 'teste12345'
-    })
+    const { body } = await request(app)
+      .post('/auth')
+      .send({
+        username: user?.username,
+        password: 'teste12345'
+      })
     const accessToken: string = body.accessToken
-    const responseBeforeExpiration = await request(app).get('/test')
+    const responseBeforeExpiration = await request(app)
+      .get('/test')
       .auth(accessToken, { type: 'bearer' })
       .send()
     expect(responseBeforeExpiration.status).toBe(200)
@@ -195,5 +221,17 @@ describe('Express - Auth', () => {
     expect(responseAfterExpiration.status).toBe(200)
 
     jest.useRealTimers()
+  })
+
+  it('should get github access-token', async () => {
+    const code = 'teste'
+
+    const githubResponse = await request(app)
+      .post('/auth/github')
+      .send({ code })
+
+    expect(auth).toHaveBeenCalledTimes(1)
+    expect(auth).toHaveBeenCalledWith(code)
+    expect(githubResponse.status).toBe(200)
   })
 })
--- src/infra/http/express/routes/auth/index.ts
+++ src/infra/http/express/routes/auth/index.ts
@@ -2,17 +2,19 @@ import { Request, Response, Router } from 'express'
 
 import { AuthUserUseCase } from '@application/user/auth/auth-user.use-case'
 import { UserRepository } from '@infra/db/user'
+import auth from '@infra/github/auth'
 
 const authRoute = Router()
 
 const userRepository = new UserRepository()
 const authUseCase = new AuthUserUseCase(userRepository)
 
-authRoute.post('/auth', async (req: Request, res: Response) => {
+authRoute.post('/', async (req: Request, res: Response) => {
   const { username, email, password } = req.body
-  const tokens = username === undefined
-    ? await authUseCase.authByEmail(email, password)
-    : await authUseCase.authByUsername(username, password)
+  const tokens =
+    username === undefined
+      ? await authUseCase.authByEmail(email, password)
+      : await authUseCase.authByUsername(username, password)
 
   res.status(200).json(tokens)
 })
@@ -23,6 +25,12 @@ authRoute.post('/refresh-token', async (req: Request, res: Response) => {
   const tokens = await getUseCase.authByRefreshToken(refreshToken)
 
   res.status(200).json(tokens)
+})
+
+authRoute.post('/github', async (req: Request, res: Response) => {
+  const { code } = req.body
+  const github = await auth(code)
+  res.status(200).json(github)
 })
 
 export default authRoute

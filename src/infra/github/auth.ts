d98b3db0d0132233d8cfaf94e6a13a98fabacb74
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-16 13:45:10 -0300

    return access-token only

--- src/infra/github/auth.ts
+++ src/infra/github/auth.ts
@@ -4,13 +4,17 @@ import axios from 'axios'
 export default async (code: string) => {
   const GITHUB_AUTH_URL = 'https://github.com/login/oauth/access_token'
   const params = {
-    client_id: env('GITHUB_CLIENT_ID'),
-    client_secret: env('GITHUB_CLIENT_SECRET'),
     code,
-    redirect_uri: env('REDIRECT_URL')
+    redirect_uri: env('REDIRECT_URL'),
+    client_id: env('GITHUB_CLIENT_ID'),
+    client_secret: env('GITHUB_CLIENT_SECRET')
   }
-  const { data: accessToken } = await axios.post(GITHUB_AUTH_URL, params, {
-    headers: { 'Accept': 'application/json' }
-  })
-  return accessToken
+  try {
+    const { data } = await axios.post(GITHUB_AUTH_URL, params, {
+      headers: { Accept: 'application/json' }
+    })
+    return data.access_token
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message)
+  }
 }

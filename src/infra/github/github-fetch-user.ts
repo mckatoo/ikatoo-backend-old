452360510f4be600bc5d6081d231c5583377cecf
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-22 22:41:32 -0300

    rename fetch user function

--- src/infra/github/github-fetch-user.ts
+++ src/infra/github/github-fetch-user.ts
@@ -1,8 +1,11 @@
+import { GetUserOutput } from '@application/user/get/get-user.use-case'
 import githubApi from './github-api'
 
 export default async (accessToken: string) => {
   try {
-    const { data: user } = await githubApi(accessToken).get('user')
+    const { data: user } = await githubApi(accessToken).get<GetUserOutput>(
+      '/user'
+    )
     return user
   } catch (error) {
     if (error instanceof Error) throw new Error(error.message)

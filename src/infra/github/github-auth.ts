3c6aaf67ddee5e8f748045aa8bc4ca5edacdfae1
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-26 11:31:10 -0300

    typed github-auth

--- src/infra/github/github-auth.ts
+++ src/infra/github/github-auth.ts
@@ -1,6 +1,12 @@
 import { env } from '@infra/env'
 import axios from 'axios'
 
+interface GithubOauthResponse {
+  access_token: string
+  scope: string
+  token_type: string
+}
+
 export default async (code: string) => {
   const GITHUB_AUTH_URL = 'https://github.com/login/oauth/access_token'
   const params = {
@@ -10,9 +16,13 @@ export default async (code: string) => {
     client_secret: env('GITHUB_CLIENT_SECRET')
   }
   try {
-    const { data } = await axios.post(GITHUB_AUTH_URL, params, {
-      headers: { Accept: 'application/json' }
-    })
+    const { data } = await axios.post<GithubOauthResponse>(
+      GITHUB_AUTH_URL,
+      params,
+      {
+        headers: { Accept: 'application/json' }
+      }
+    )
     return data.access_token
   } catch (error) {
     if (error instanceof Error) throw new Error(error.message)

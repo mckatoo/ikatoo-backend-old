159d6c6128b6b3e6c1b940bdced77c46172c1263
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-28 20:11:28 -0300

    🏷️ (chore): data type for github user request

--- src/infra/github/github-fetch-user.ts
+++ src/infra/github/github-fetch-user.ts
@@ -1,15 +1,9 @@
-import { GetUserOutput } from '@application/user/get/get-user.use-case'
+import { GithubUser } from '@infra/http/types/Github'
 import githubApi from './github-api'
-
-type GithubResponse = Omit<GetUserOutput, 'username'> & {
-  login: string
-}
 
 export default async (accessToken: string) => {
   try {
-    const { data: user } = await githubApi(accessToken).get<GithubResponse>(
-      '/user'
-    )
+    const { data: user } = await githubApi(accessToken).get<GithubUser>('/user')
     return user
   } catch (error) {
     if (error instanceof Error) throw new Error(error.message)

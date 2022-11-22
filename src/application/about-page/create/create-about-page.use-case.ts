05d62d0c196f82ac457d0191368bf7c3721c5f3c
Milton Carlos Katoo <mckatoo@gmail.com>
2022-11-22 12:39:03 -0300

    removed unnecessary type

--- src/application/about-page/create/create-about-page.use-case.ts
+++ src/application/about-page/create/create-about-page.use-case.ts
@@ -1,14 +1,12 @@
 import { AboutPage, AboutPageProps } from '@domain/about/about-page.entity'
-import { AboutPageRepositoryInterface } from '@domain/about/about-page.repository'
-
-type CreateAboutPageInput = AboutPageProps & { id?: string }
+import { AboutPageRepositoryInterface, AboutPageWithId } from '@domain/about/about-page.repository'
 
 type CreateAboutPageOutput = AboutPageProps & { id: string }
 
 export class CreateAboutPageUseCase {
   constructor (private readonly aboutPageRepository: AboutPageRepositoryInterface) {}
 
-  async execute (input: CreateAboutPageInput): Promise<CreateAboutPageOutput> {
+  async execute (input: AboutPageWithId): Promise<CreateAboutPageOutput> {
     const aboutPage = AboutPage.create(input, input.id)
     await this.aboutPageRepository.create(aboutPage)
 

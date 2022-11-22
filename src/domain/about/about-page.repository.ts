03f66fa95538bc8bc016d539081418ec9e12230a
Milton Carlos Katoo <mckatoo@gmail.com>
2022-11-22 12:02:40 -0300

    add update method on interface

--- src/domain/about/about-page.repository.ts
+++ src/domain/about/about-page.repository.ts
@@ -1,9 +1,11 @@
 import { AboutPageProps } from './about-page.entity'
 
 export type AboutPageWithId = AboutPageProps & { id?: string }
+export type AboutPageUpdateType = Partial<AboutPageProps> & { id: string }
 
 export interface AboutPageRepositoryInterface {
   create: (aboutPage: AboutPageWithId) => Promise<void>
+  update: (aboutPage: AboutPageUpdateType) => Promise<void>
   getByUserId: (userId: string) => Promise<AboutPageWithId>
   getAll: () => Promise<AboutPageWithId[]>
 }
